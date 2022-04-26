b2a2b3d6e562c81ec4e7591512b140ffa298ffd3
mohammadshariful <mohammadsharifulislam64@gmail.com>
2022-04-26 16:53:58 +0600

    footer component add

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,9 +1,12 @@
 import React from "react";
 import "./Footer.css";
 const Footer = () => {
+  const date = new Date();
+  const fullYear = date.getFullYear();
   return (
-    <div>
-      <h2>Footer component</h2>
+    <div className="bg-dark text-white py-3 mt-4 text-center">
+      <p>&copy; copyright {fullYear} all rights reserved.</p>
+      <h6>Development by &hearts; Mohammad Shariful</h6>
     </div>
   );
 };
