192b958c4585380b7108cdd56dd00e6e5a06a024
mohammadshariful <mohammadsharifulislam64@gmail.com>
2022-04-28 02:21:25 +0600

    implement of axios

--- src/Hooks/useServices.js
+++ src/Hooks/useServices.js
@@ -1,11 +1,28 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 
-const useServices = () => {
+const useServices = ({ page, size }) => {
   const [services, setServices] = useState([]);
   const [isLoading, setLoading] = useState(false);
   useEffect(() => {
+    const url = `https://benevolent2022.herokuapp.com/services?page=${page}&size=${size}`;
     setLoading(true);
-    fetch("https://benevolent2022.herokuapp.com/services")
+    const getService = async () => {
+      try {
+        const { data } = await axios.get(url);
+        setServices(data);
+        setLoading(false);
+      } catch (error) {
+        setLoading(false);
+      }
+    };
+    getService();
+  }, [page, size]);
+  /*  useEffect(() => {
+    setLoading(true);
+    fetch(
+      `https://benevolent2022.herokuapp.com/services?page=${page}&size=${size}`
+    )
       .then((res) => res.json())
       .then((data) => {
         setServices(data);
@@ -14,7 +31,7 @@ const useServices = () => {
       .catch((error) => {
         setLoading(false);
       });
-  }, []);
+  }, [page, size]); */
   return { services, isLoading, setServices };
 };
 
--- src/Pages/Home/Supports/Supports.js
+++ src/Pages/Home/Supports/Supports.js
@@ -1,11 +1,29 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { Container, Row } from "react-bootstrap";
 import useServices from "../../../Hooks/useServices";
 import Loading from "../../Shared/Loading/Loading";
 import SingleSupport from "../SingleSupport/SingleSupport";
 import "./Supports.css";
 const Supports = () => {
-  const { services, isLoading } = useServices();
+  const [pageCount, setPageCount] = useState(0);
+  const [page, setPage] = useState(0);
+  const [size, setSize] = useState(6);
+  const { services, isLoading } = useServices({ page, size });
+
+  /* ------------------------------------------------------ */
+  useEffect(() => {
+    const count = async () => {
+      const { data } = await axios.get(
+        "https://benevolent2022.herokuapp.com/servicesCount"
+      );
+      const count = data.count;
+      const page = Math.ceil(count / 6);
+      setPageCount(page);
+    };
+    count();
+  }, []);
+
   return (
     <>
       {isLoading ? (
@@ -17,6 +35,24 @@ const Supports = () => {
               <SingleSupport key={service._id} service={service} />
             ))}
           </Row>
+          <div className="pagination-container text-center">
+            {[...Array(pageCount).keys()].map((number, index) => (
+              <button
+                key={index}
+                className={page === number ? "selected" : ""}
+                onClick={() => setPage(number)}
+              >
+                {number + 1}
+              </button>
+            ))}
+            <select onChange={(e) => setSize(e.target.value)}>
+              <option value="5" selected>
+                5
+              </option>
+
+              <option value="10">10</option>
+            </select>
+          </div>
         </Container>
       )}
     </>
